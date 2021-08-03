2ff1fefbebaa6eedaa7b18b7d9bbf213773715d4
fanxiaobin <xiaobin.fan@archermind.com>
2021-08-03 15:38:50 +0800

    father plan

---
+++ quotation-auto-generated/src/main/java/com/wantbchack/quotation/util/ResultUtil.java
@@ -0,0 +1,39 @@
+package com.wantbchack.quotation.util;
+
+
+import com.wantbchack.quotation.dto.Result;
+import com.wantbchack.quotation.enums.ResultEnum;
+
+public class ResultUtil {
+    /**成功且带数据**/
+    public static Result success(Object object){
+        Result result = new Result();
+        result.setCode(ResultEnum.SUCCESS.getCode());
+        result.setMsg(ResultEnum.SUCCESS.getMsg());
+        result.setData(object);
+        return result;
+    }
+
+    /**成功且带数据**/
+    public static Result success(Object object,String msg){
+        Result result = new Result();
+        result.setCode(ResultEnum.SUCCESS.getCode());
+        result.setMsg(msg);
+        result.setData(object);
+        return result;
+    }
+    /**成功但不带数据**/
+    public static Result success(){
+
+        return success(null);
+    }
+    /**失败**/
+    public static Result error(Integer code,String msg){
+        Result result = new Result();
+        result.setCode(code);
+        result.setMsg(msg);
+        return result;
+    }
+
+
+}
