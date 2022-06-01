e5b53bb30e13117db544d017e525a4c060b359e8
wantbchack <62879050+wantbchack@users.noreply.github.com>
2022-06-01 15:41:28 +0800

    Update demoController.java

--- demo-init/src/main/java/com/wantbchack/business/controller/demoController.java
+++ demo-init/src/main/java/com/wantbchack/business/controller/demoController.java
@@ -6,6 +6,7 @@ import com.wantbcahck.common.dto.business.MyReq;
 import com.wantbcahck.common.util.ResultUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.wantbchack.business.annotation.Myannotaiton;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ public class demoController {
 
 
     @PostMapping("/list")
+    @Myannotaiton
     public Result list(@RequestBody(required = false) MyReq req){
         return ResultUtil.success(req,"程序正常运行!!!!!");
     }
