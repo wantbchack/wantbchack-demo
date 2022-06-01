af2a486d8e0af1d20de356dfbcbdabb2a915c324
wantbchack <62879050+wantbchack@users.noreply.github.com>
2022-06-01 15:39:36 +0800

    Create Myannotaiton.java

---
+++ demo-init/src/main/java/com/wantbchack/business/annotation/Myannotaiton.java
@@ -0,0 +1,16 @@
+package com.wantbchack.business.annotation;
+
+
+
+import java.lang.annotation.*;
+
+/**
+ * Create by  fanxiaobin
+ * Date 2022/6/1  10:55
+ * Description
+ */
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Myannotaiton {
+}
